ebe627c7926e7e47262df9c3f6f8e5d91aa055e1
Lalu423 <>
2023-04-20 21:55:26 -0400

    added description comments to code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 $(function () {
-
+//created a var to call dayjs
   var today = dayjs();
-
+//This will display the current day. 
   $('#currentDay').text(today.format('dddd, MMMM D YYYY'));
- 
+ //This is the save button function that will save text in the text area. 
   $('.saveBtn').on('click', function () {
     const key = $(this).parent().attr('id');
     const value = $(this).siblings('.description').val();
@@ -14,13 +14,13 @@ $(function () {
     //Get current time
     var currentTime = dayjs().format('H')
     console.log("currentTime: ", currentTime)
-    //Checking for each hour
+    //this checks for the current hour
     $('.time-block').each(function () {
-      // get time block
+      // This will get the time block
       const key = parseInt($(this).attr('id').split('-')[1]);
       console.log("key: ", key)
 
-      // Compare time block to current time
+      // This will compare time blocks and toggle the classes to represent the current hour in the correct color format.
       if (key === currentTime) {
         $(this).removeClass('past future').addClass('present')
 
@@ -32,19 +32,16 @@ $(function () {
 
 
     });
-
-
-
-
   }
+  //This calls the hour checker function. 
   hourChecker();
- 
+ //This loads the saved text in the corresponding text field it was saved in. 
   $('.time-block').each(function () {
     const key = $(this).attr('id');
     const value = localStorage.getItem(key);
     $(this).children('.description').val(value);
   });
-
+//This will check the hour every second. 
   hourChecker();
   setInterval(updateTime, 1000);
   
